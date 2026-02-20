92cae5861d16a09dcb0bc8842bde9dbf13a533c6
Pingitzer Dávid <davidpingitzer7@gmail.com>
2026-02-20 13:38:55 +0100

    Thread syncing fix

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,15 @@ public class Main extends Thread {
     private final File file;
 
     public Main(File file) {
+        Accessor.getInstance().clearTable();
+        Pool.getInstance().clearPools();
         this.file = file;
     }
 
     public void run() {
         Accessor.getInstance().clearTable();
         Pool.getInstance().clearPools();
+        System.out.println("Interpreter: "+Pool.getInstance().getOutputStream());
         Divider source = new Divider(file.getPath());
         Register engine = new Register(source.getCodeBase());
         try {
